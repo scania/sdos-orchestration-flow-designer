a688544b20c168e4ece48bf7f0d0f452237bfa2a
SMEDJ8 <saireddy.medapati@scania.com>
2023-11-01 16:20:41 +0530

    Refactor landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -60,48 +60,20 @@ function App() {
   return (
     <div className={`App`}>
       <main className={styles.main}>
-        <div className={styles.header}>
-          <div className={styles["header__project"]}>
-            <div className={styles["header__project-heading"]}>
-              <h1>
-                ORCHESTRATION FLOW
-                <br /> DESIGNER{" "}
-              </h1>
-            </div>
-
-            <div className={styles["header__project-summary"]}>
-              Open and edit your orchestration flow graph or create a new one.
-              <br />
-              You can also view what graph are available to you from other
-              <br /> projects
-            </div>
-          </div>
-
-          <div className={styles["header__info"]}>
-            <div className={styles["header_info"]}>
-              <h6 className={styles["header__info__user-name"]}>
-                {session?.user?.name?.toLocaleUpperCase()}
-              </h6>
-              <h6 className={styles["header__info__user-role"]}> ADMIN </h6>
-            </div>
-
-            <Link href="help">
-              <div className={styles["header__info-help"]}>
-                <tds-icon name="info" size="20"></tds-icon>
-                Need Help?
-              </div>
-            </Link>
-          </div>
-        </div>
         <div className={styles.tabs}>
           <Tabs>
-            <Panel title="Your Work"></Panel>
+            <Panel title="My Work"></Panel>
             <Panel title="All Designs"></Panel>
-            <Panel title="Support"></Panel>
           </Tabs>
         </div>
         <div className={styles.content}>
-          <h2 className={styles["content__heading"]}>In Focus</h2>
+          <div className={styles["header__project-summary"]}>
+            <h2 className={styles["content__heading"]}>My Work</h2>
+            Open and edit your orchestration flow graph or create a new one.
+            <br />
+            You can also view what graph are available to you from other
+            <br /> projects
+          </div>
           <div className={styles["content__main"]}>
             <div className={styles["content__main__buttons"]}>
               <Button
@@ -160,6 +132,19 @@ function App() {
                 <tds-icon name="redirect" size="16px"></tds-icon>
               </Button>
             </div>
+
+            <h2 className={styles["content__headingcontent"]}>In Focus</h2>
+
+            <div className={styles["content__main__cards"]}>
+              {renderCards()}
+            </div>
+
+            <div className={styles["content__main__line"]}>
+              <tds-divider orientation="horizontal"></tds-divider>
+            </div>
+
+            <h2 className={styles["content__headingcontent"]}>Other</h2>
+
             <div className={styles["content__main__cards"]}>
               {renderCards()}
             </div>
