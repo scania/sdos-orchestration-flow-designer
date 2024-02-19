b64246ca4edbeec2bba18e2a3eb9391cc491bba0
akul8b <abhishek.kundu@scania.com>
2024-02-19 20:37:44 +0530

    changed form name and helper text

--- src/pages/ofd/DynamicForm.tsx
+++ src/pages/ofd/DynamicForm.tsx
@@ -1,136 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import { useForm, SubmitHandler } from "react-hook-form";
-
-// interface IClassConfig {
-//   [key: string]: any;
-// }
-
-// interface IFormInput {
-//   [key: string]: any;
-// }
-
-// interface DynamicFormProps {
-//   classConfig: IClassConfig;
-//   formData: any;
-//   onSubmit: (data: IFormInput) => void;
-//   onClose: () => void;
-//   excludeKeys: string[];
-//   label: string;
-// }
-
-// const textAreaKeys = ["iris:constructSparql"];
-
-// const DynamicForm: React.FC<DynamicFormProps> = ({
-//   classConfig = {},
-//   formData = [],
-//   onSubmit,
-//   excludeKeys,
-//   label,
-//   onClose,
-// }) => {
-//   const { register, handleSubmit, reset, formState } = useForm<IFormInput>();
-//   const { errors } = formState;
-
-//   //   Update form state when classConfig changes
-//   useEffect(() => {
-//     reset(classConfig);
-//   }, [classConfig, reset]);
-
-//   const handleFormSubmit: SubmitHandler<IFormInput> = (data) => {
-//     onSubmit(data);
-//   };
-//   console.log(formData, formData);
-
-//   const renderInputField = (key: string, value: any) => {
-//     const isObject = typeof value === "object" && value !== null;
-
-//     if (isObject) {
-//       return Object.entries(value).map(([subKey, subValue]) => (
-//         <section key={`${key}.${subKey}`} className="form-section">
-//           <tds-text-field
-//             className="tds-text-field"
-//             state={errors[key] ? "error" : "default"}
-//             label-position="outside"
-//             label={`${key} ${subKey}`}
-//             // @ts-ignore
-//             helper={errors[key] ? errors[key][subKey].message : ""}
-//             {...register(`${key}.${subKey}`, {
-//               required: "This field is required",
-//               minLength: { value: 2, message: "Minimum length is 2" },
-//               maxLength: { value: 50, message: "Maximum length is 50" },
-//             })}
-//           />
-//         </section>
-//       ));
-//     } else {
-//       return (
-//         <section key={key} className="form-section">
-//           {textAreaKeys.includes(key) ? (
-//             <tds-textarea
-//               className="tds-text-field"
-//               label={key}
-//               label-position="outside"
-//               state={errors[key] ? "error" : "default"}
-//               // @ts-ignore
-//               helper={errors[key] ? errors[key].message : ""}
-//               {...register(key, {
-//                 required: "This field is required",
-//                 minLength: { value: 2, message: "Minimum length is 2" },
-//               })}
-//             />
-//           ) : (
-//             <tds-text-field
-//               className="tds-text-field"
-//               label={key}
-//               label-position="outside"
-//               state={errors[key] ? "error" : "default"}
-//               // @ts-ignore
-//               helper={errors[key] ? errors[key].message : ""}
-//               {...register(key, {
-//                 required: "This field is required",
-//                 minLength: { value: 2, message: "Minimum length is 2" },
-//                 maxLength: { value: 50, message: "Maximum length is 50" },
-//               })}
-//             />
-//           )}
-//         </section>
-//       );
-//     }
-//   };
-
-//   return (
-//     <>
-//       <h4 style={{ marginBottom: "5px" }}>Edit Class</h4>
-//       <tds-chip type="button" size="sm">
-//         <span slot="label" style={{ fontWeight: 700, fontSize: "10px" }}>
-//           {label}
-//         </span>
-//       </tds-chip>
-//       <article className="form" style={{ marginTop: "10px" }}>
-//         <form onSubmit={handleSubmit(handleFormSubmit)}>
-//           {Object.entries(classConfig)
-//             .filter(([key]) => !excludeKeys.includes(key))
-//             .map(([key, value]) => renderInputField(key, value))}
-//           <section
-//             style={{ display: "flex", gap: "5px", justifyContent: "right" }}
-//           >
-//             <tds-button
-//               type="button"
-//               size="sm"
-//               variant="secondary"
-//               text="Close"
-//               onClick={onClose}
-//             ></tds-button>
-//             <tds-button type="submit" size="sm" text="Apply"></tds-button>
-//           </section>
-//         </form>
-//       </article>
-//     </>
-//   );
-// };
-
-// export default React.memo(DynamicForm);
-
 import React, { useEffect } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 
@@ -182,7 +49,6 @@ const DynamicForm: React.FC<DynamicFormProps> = ({
 }) => {
   const { register, handleSubmit, reset, formState } = useForm<IFormInput>();
   const { errors } = formState;
-
   // Update form state when formData changes
   useEffect(() => {
     const defaultValues = formData.reduce((acc, field) => {
@@ -198,7 +64,7 @@ const DynamicForm: React.FC<DynamicFormProps> = ({
 
   const renderInputField = (field: FormField) => {
     const { name, type, label, validation } = field;
-    const isTextArea = type === "textarea";
+    // const isTextArea = type === "textarea";
     const validationRules = {
       required: validation.required ? "This field is required" : false,
       minLength: validation.min
@@ -220,28 +86,19 @@ const DynamicForm: React.FC<DynamicFormProps> = ({
           }
         : undefined,
     };
+    const fieldName = name.split("/");
 
     return (
       <section key={name} className="form-section">
-        {isTextArea ? (
-          <tds-textarea
-            className="tds-text-field"
-            label={label}
-            label-position="outside"
-            state={errors[name] ? "error" : "default"}
-            helper={errors[name]?.message}
-            {...register(name, validationRules)}
-          />
-        ) : (
-          <tds-text-field
-            className="tds-text-field"
-            label={label}
-            label-position="outside"
-            state={errors[name] ? "error" : "default"}
-            helper={errors[name]?.message}
-            {...register(name, validationRules)}
-          />
-        )}
+        <tds-textarea
+          className="tds-text-field"
+          label={fieldName[fieldName.length - 1]}
+          label-position="outside"
+          state={errors[name] ? "error" : "default"}
+          helper={errors[name]?.message}
+          placeholder={label}
+          {...register(name, validationRules)}
+        />
       </section>
     );
   };
