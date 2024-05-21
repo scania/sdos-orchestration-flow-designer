1fff8b10502aa3a256f7296d4777e96d3ed60485
Kundu Abhishek <abhishek.kundu@scania.com>
2024-05-21 13:00:16 +0200

    reading the test credentials form cypress test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,23 @@
-
 import { defineConfig } from "cypress";
 import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
 import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
 import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
+require("dotenv").config({ path: "./.env.local" });
+import { env } from "./src/lib/env";
 
 async function setupNodeEvents(
   on: Cypress.PluginEvents,
   config: Cypress.PluginConfigOptions
 ): Promise<Cypress.PluginConfigOptions> {
+  config.env = {
+    TEST_USERNAME: env.TEST_USERNAME,
+    TEST_PASSWORD: env.TEST_PASSWORD,
+  };
+
   // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
   await addCucumberPreprocessorPlugin(on, config);
-
   // Include plugins
-  require('cypress-mochawesome-reporter/plugin')(on);
+  require("cypress-mochawesome-reporter/plugin")(on);
 
   on(
     "file:preprocessor",
@@ -41,17 +46,16 @@ export default defineConfig({
 
     charts: true,
     code: false,
-    embeddedScreenshots: true, 
-    inlineAssets: true
+    embeddedScreenshots: true,
+    inlineAssets: true,
   },
-
 
   // Specific for e2e tests
   e2e: {
     setupNodeEvents,
     specPattern: "cypress/features/**/*.feature",
-    baseUrl: "https://ofd.sdip.devtest.aws.scania.com",
+    baseUrl: env.TEST_BASE_URL,
     experimentalModifyObstructiveThirdPartyCode: true,
-    chromeWebSecurity: false
-  }
+    chromeWebSecurity: false,
+  },
 });
