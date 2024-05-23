ac58a83594942243c1fc00a9f7fffa43a98413de
Kundu Abhishek <abhishek.kundu@scania.com>
2024-05-24 01:31:30 +0200

    added test cases for ofd.feature

--- cypress/support/step_definitions/pages/ofd.ts
+++ cypress/support/step_definitions/pages/ofd.ts
@@ -1,4 +1,4 @@
-import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
+import { When, Given, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 Given("OFD page is in view", () => {
   cy.visit("/ofd/test");
@@ -10,3 +10,65 @@ Then("task node should be visible", () => {
     .should("be.visible")
     .should("contain.text", "Task");
 });
+
+Then("Primary Mode classes should be visible", () => {
+  cy.contains('div[draggable="true"] span', "HTTP Action").should("exist");
+});
+
+When("Navigated to Parameters Tab", () => {
+  cy.contains("li", "Parameters").click({ force: true });
+});
+
+Then("Parameters classes should be present", () => {
+  cy.contains('div[draggable="true"] span', "Standard Parameter").should(
+    "exist"
+  );
+});
+
+When("Navigated to Scripts Tab", () => {
+  cy.contains("li", "Scripts").click({ force: true });
+});
+
+Then("Script classes should be present", () => {
+  cy.contains('div[draggable="true"] span', "Groovy Script").should("exist");
+});
+
+When("a partial class name containing ttp is typed in the search field", () => {
+  cy.get(".text-field-input").clear().type("ttp");
+});
+
+Then("HTTP Action should appear in the list regardless of tab", () => {
+  cy.contains('div[draggable="true"] span', "HTTP Action").should("exist");
+});
+
+When("a partial class name containing ult is typed in the search field", () => {
+  cy.get(".text-field-input").clear().type("ult");
+});
+
+Then("Result Action should appear in the list regardless of Tab", () => {
+  cy.contains('div[draggable="true"] span', "Result Action").should("exist");
+});
+
+When(
+  "HTTP Action class in dragged and dropped from the sidebar to the canvas",
+  () => {
+    cy.contains('div[draggable="true"]', "HTTP Action").then(($el) => {
+      const coords = $el[0].getBoundingClientRect();
+      cy.log(`Element initial position: (${coords.left}, ${coords.top})`);
+
+      cy.wrap($el)
+        .trigger("mousedown")
+        .trigger("mousemove", {
+          clientX: coords.left + 5,
+          clientY: coords.top + 5,
+        })
+        .trigger("mousemove", { pageX: 883, pageY: 571 })
+        .trigger("mouseup", { force: true });
+
+      cy.log("Drag and drop action performed");
+
+      // Verify the element has moved to the target location
+      cy.get(".target-container").should("contain.text", "HTTP Action");
+    });
+  }
+);
