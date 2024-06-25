d2186d9370523866b9f7dc3beb2c9e57a19ca2b3
Kundu Abhishek <abhishek.kundu@scania.com>
2024-06-25 09:10:15 +0200

    workflow update

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,8 @@
-FROM node:16
+FROM node:20
 WORKDIR /app
-ARG NEXT_PUBLIC_API_BASE_URL
-# Set the environment variable from the build argument (required for static content)
-ENV NEXT_PUBLIC_API_BASE_URL=${NEXT_PUBLIC_API_BASE_URL}
 COPY . ./
-RUN npm config set update-notifier false
-RUN rm -f package-lock.json
+# RUN npm config set update-notifier false
+# RUN rm -f package-lock.json
 RUN npm install
 RUN npm run build
 EXPOSE 3000
--- src/lib/env.js
+++ src/lib/env.js
@@ -1,14 +1,14 @@
 const { z } = require("zod");
 
 const envSchema = z.object({
-  OFD_AZURE_AD_CLIENT_ID: z.string().uuid().nonempty(),
-  OFD_AZURE_AD_CLIENT_SECRET: z.string().nonempty(),
-  OFD_AZURE_AD_TENANT_ID: z.string().uuid().nonempty(),
-  STARDOG_USERNAME: z.string().nonempty(),
-  STARDOG_PASSWORD: z.string().nonempty(),
-  STARDOG_ENDPOINT: z.string().url().nonempty(),
-  NEXTAUTH_URL: z.string().url().nonempty(),
-  NEXTAUTH_SECRET: z.string().uuid().nonempty(),
+  OFD_AZURE_AD_CLIENT_ID: z.string().uuid().optional(),
+  OFD_AZURE_AD_CLIENT_SECRET: z.string().optional(),
+  OFD_AZURE_AD_TENANT_ID: z.string().uuid().optional(),
+  STARDOG_USERNAME: z.string().optional(),
+  STARDOG_PASSWORD: z.string().optional(),
+  STARDOG_ENDPOINT: z.string().url().optional(),
+  NEXTAUTH_URL: z.string().url().optional(),
+  NEXTAUTH_SECRET: z.string().uuid().optional(),
   LOG_LEVEL: z.string().optional(),
   NODE_ENV: z.enum(["development", "test", "production"]).optional(),
 });
