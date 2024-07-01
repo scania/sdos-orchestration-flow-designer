f3872b7a34d0af57ec70cfd94a1ab03568779d23
abhishekkundu25 <168546452+abhishekkundu25@users.noreply.github.com>
2024-07-01 11:16:46 +0200

    Added CI pipeline for build and publish (#17)
    * workflow update
    
    * publish now works only for tags
    
    * no env validation during build

--- src/lib/env.js
+++ src/lib/env.js
@@ -1,19 +1,28 @@
 const { z } = require("zod");
 
 const envSchema = z.object({
-  OFD_AZURE_AD_CLIENT_ID: z.string().uuid().nonempty(),
-  OFD_AZURE_AD_CLIENT_SECRET: z.string().nonempty(),
-  OFD_AZURE_AD_TENANT_ID: z.string().uuid().nonempty(),
-  STARDOG_USERNAME: z.string().nonempty(),
-  STARDOG_PASSWORD: z.string().nonempty(),
-  STARDOG_ENDPOINT: z.string().url().nonempty(),
-  NEXTAUTH_URL: z.string().url().nonempty(),
-  NEXTAUTH_SECRET: z.string().uuid().nonempty(),
+  OFD_AZURE_AD_CLIENT_ID: z.string().uuid().min(1),
+  OFD_AZURE_AD_CLIENT_SECRET: z.string().min(1),
+  OFD_AZURE_AD_TENANT_ID: z.string().uuid().min(1),
+  STARDOG_USERNAME: z.string().min(1),
+  STARDOG_PASSWORD: z.string().min(1),
+  STARDOG_ENDPOINT: z.string().url().min(1),
+  NEXTAUTH_URL: z.string().url().min(1),
+  NEXTAUTH_SECRET: z.string().uuid().min(1),
   LOG_LEVEL: z.string().optional(),
   NODE_ENV: z.enum(["development", "test", "production"]).optional(),
 });
 
-const env = envSchema.parse(process.env);
+//This will make all env vars optional
+const buildEnvSchema = envSchema.partial();
+let env;
+
+//making all vars optional during build and as per envSchema during start
+if (process.env.npm_lifecycle_event === "build") {
+  env = buildEnvSchema.parse(process.env);
+} else {
+  env = envSchema.parse(process.env);
+}
 
 module.exports = {
   env,
