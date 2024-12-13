d6222b308e066a2db3285be3e840b572d5edd186
Mitrasinovic Goran <goran.mitrasinovic@scania.com>
2024-12-13 10:31:47 +0100

    added randomized value

--- src/helpers/helper.tsx
+++ src/helpers/helper.tsx
@@ -1,1 +1,5 @@
 export const replaceSpecialChars = (str: string) => str.replace(/[/.]/g, "_");
+
+export const randomizeValue = (value: number) => {
+    return value * (0.85 + Math.random() * 0.3)
+}
