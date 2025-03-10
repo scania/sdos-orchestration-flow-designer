6a17d162dbe4b5636e16c4901ed1f1cb1ec0fdf2
Mitrasinovic Goran <goran.mitrasinovic@scania.com>
2025-03-10 09:57:52 +0100

    user preferences

--- src/components/ActionToolbar/ActionToolbar.tsx
+++ src/components/ActionToolbar/ActionToolbar.tsx
@@ -1,7 +1,11 @@
-import styles from "./ActionToolbar.module.scss";
+
+import { useState } from "react";
 import { useRouter } from "next/router";
-import GraphOptions from "../GraphOptions/GraphOptions";
+import styles from "./ActionToolbar.module.scss";
 import Tooltip from "../Tooltip/Tooltip";
+import GraphOptions from "../GraphOptions/GraphOptions";
+import Modal from "../Modal/CustomModal";
+import UserPreferences from "../UserPreferences/UserPreferences"
 
 type Graph = {
   name?: string;
@@ -28,6 +32,9 @@ const ActionToolbar: React.FC<AccordionProps> = ({
 }) => {
   const router = useRouter();
 
+  const [userPreferencesModalIsOpen, setUserPreferencesModalIsOpen] =
+    useState(false);
+
   return (
     <div className={styles.container}>
       <div className="pointer" onClick={router.back}>
@@ -39,8 +46,18 @@ const ActionToolbar: React.FC<AccordionProps> = ({
           name="back"
         ></tds-icon>
       </div>
-      {toolbar && (
-        <div className={styles.container__actionsContainer}>
+      {toolbar && 
+      <div className={styles.container__actionsContainer}>
+        <Modal
+          isOpen={userPreferencesModalIsOpen}
+          onRequestClose={() => setUserPreferencesModalIsOpen(false)}
+          title="User preferences"
+        >
+          <UserPreferences/>
+        </Modal>
+          <span onClick={() => setUserPreferencesModalIsOpen(true)} className="pointer">
+            User Preferences
+          </span>
           <GraphOptions
             selector="#graph-options"
             graphDescription={graph?.description || ""}
@@ -86,7 +103,7 @@ const ActionToolbar: React.FC<AccordionProps> = ({
             </>
           ) : null}
         </div>
-      )}
+      }
     </div>
   );
 };
