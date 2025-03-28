1a57484611a153c45b199abe962323e2a4e2ffe6
gmirmz <goran.mitrasinovic@scania.com>
2025-03-28 17:27:21 +0100

    Deselect selectedNode when deleting it

--- src/components/ActionsMenu/ActionsMenu.tsx
+++ src/components/ActionsMenu/ActionsMenu.tsx
@@ -1,15 +1,20 @@
 import styles from "./actionsMenu.module.scss";
 
-const ActionsMenu = ({onDeleteClick, onDisconnectClick}) => {
+const ActionsMenu = ({ onDeleteClick, onDisconnectClick }) => {
   return (
-  <div className={styles.container}>
-    <ul className={styles.container__list}>
-      <li className={`${styles.container__disabled} ${styles.container__list__item}`}>Rename</li>
-      <li className={`${styles.container__disabled} ${styles.container__list__item}`}>Duplicate</li>
-      <li onClick={onDisconnectClick} className={`${styles.container__list__item}`}>Disconnect</li>
-      <li onClick={onDeleteClick} className={styles.container__list__item}>Delete</li>
-    </ul>
-  </div>
-)};
+    <div className={styles.container} onClick={(e) => e.stopPropagation()} >
+      <ul className={styles.container__list}>
+        <li className={`${styles.container__disabled} ${styles.container__list__item}`}>Rename</li>
+        <li className={`${styles.container__disabled} ${styles.container__list__item}`}>Duplicate</li>
+        <li onClick={onDisconnectClick} className={`${styles.container__list__item}`}>Disconnect</li>
+        {/* Prevent the node-click on deleting a node */}
+        <li onClick={(e) => {
+          e.stopPropagation();
+          onDeleteClick();
+        }} className={styles.container__list__item}>Delete</li>
+      </ul>
+    </div>
+  )
+};
 
 export default ActionsMenu;
--- src/store/ofdStore.ts
+++ src/store/ofdStore.ts
@@ -4,6 +4,19 @@ interface Edge {
   id: string;
 }
 
+interface Node {
+  id: string;
+  type: string;
+  data,
+  position, 
+  sourcePosition, 
+  width, 
+  height, 
+  selected, 
+  positionAbsolute,
+  dragging
+}
+
 // Define the store type for typescript linting
 interface OfdStore {
   setupMode: boolean;
@@ -11,6 +24,8 @@ interface OfdStore {
   connectedEdgesFromNode: Edge[];
   addConnectedEdges: (edges: Edge | Edge[]) => void;
   clearConnectedEdges: () => void;
+  selectedNode: Node | null;
+  setSelectedNode: (node: Node | null) => void;
 }
 
 const useOfdStore = create<OfdStore>((set) => ({
@@ -26,6 +41,8 @@ const useOfdStore = create<OfdStore>((set) => ({
       ],
     })),
   clearConnectedEdges: () => set({ connectedEdgesFromNode: [] }),
+  selectedNode: null,
+  setSelectedNode: (node) => set({ selectedNode: node })
 }));
 
 export default useOfdStore;
