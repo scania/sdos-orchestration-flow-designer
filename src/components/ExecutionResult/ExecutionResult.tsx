c26e8e217bceea242c87b97e2a8cd08d6d258568
gmirmz <goran.mitrasinovic@scania.com>
2025-06-02 14:05:50 +0200

    JSON-view now uses theme setting

--- src/components/ExecutionResult/ExecutionResult.tsx
+++ src/components/ExecutionResult/ExecutionResult.tsx
@@ -1,11 +1,21 @@
+import { useEffect, useState } from "react";
 import JsonView from "@uiw/react-json-view";
 import { isValidJson } from "@/helpers/helper";
+import { darkTheme } from "@uiw/react-json-view/dark";
 
 const ExecutionResult = ({ executionResult }: any) => {
-  const renderJsonView = (executionResult: any) => (
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    setIsDarkTheme(theme === "dark");
+  }, []);
+
+  const renderJsonView = (data: any) => (
     <JsonView
-      value={executionResult}
+      value={data}
       indentWidth={4}
+      style={isDarkTheme ? darkTheme : undefined}
       displayDataTypes={false}
       collapsed={false}
       displayObjectSize={true}
